d4d1564e400cbff1fb04514a7b543c3fa943ab4a
Jiaxin Zhao <jzhao537@uwo.ca>
2021-01-11 14:00:17 +0800

    commit banner

--- UMallAPI/routes/index.js
+++ UMallAPI/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var db = require('../utils/db');
+var { getMsg } = require('../utils/tool');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// get banner information
+router.get('/banner',async (req,res)=>{
+  // get banner from database
+  let sql =  `SELECT * FROM banner`
+  let [err, result] = await db.query(sql)
+
+  if(!err){
+    res.send(getMsg('success', 200, result))
+  }else{
+    res.send(getMsg('fail', 500, err))
+  }
+})
 
 module.exports = router;
---
+++ UMallAPI/utils/tool.js
@@ -0,0 +1,9 @@
+function getMsg(msg, status, data){
+    return {
+        msg,
+        status,
+        data
+    }
+}
+
+exports.getMsg = getMsg;
