52b9b9a4bb4b8b910f49a73f17a1a1c69feba0a7
Jiaxin Zhao <jzhao537@uwo.ca>
2021-01-12 18:17:36 +0800

    initialize list.js

--- UMallAPI/app.js
+++ UMallAPI/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var detailRouter = require('./routes/detail');
+var listRouter = require('./routes/list');
 var { getMsg } = require('./utils/tool');
 
 var app = express();
@@ -23,7 +24,7 @@ app.use('/image_source', express.static(path.join(__dirname, 'image_source')));
 
 app.use('/', indexRouter);
 app.use('/detail', detailRouter);
-
+app.use('/list', listRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   res.send(getMsg('File Not Found', 404))
